d5566cbb0b15693f3c09c42f2e21a418bce6cd4c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-15 18:08:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_reader :name
+  attr_accessor :roster, :add_student
+  
+  hash = {}
+  
+  def initialize(name)
+    @name 
+    @roster = []
+  end
+  
+  def add_student=(student, grade)
+   @add_student = {grade => student}
+ end
+
+  
+end
