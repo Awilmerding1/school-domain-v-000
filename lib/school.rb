028cc7e1cfdcc619672a9d085fc7d09a80960650
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-15 19:09:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,8 +15,7 @@ class School
      @roster[grade] << student 
     else  
     @roster.keys.find do |grade|
-      existing_grade << grade
-    @roster[existing_grade] << student
+    
  end
 end
 end
