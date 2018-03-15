883021afb63c62b5b968c1bed2107a7a7c8717d1
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-15 19:18:09 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,9 +10,6 @@ class School
   
   def add_student(student, grade)
    @roster[grade] = []
-   array = @roster.keys
-   string = array.join
-   existing_grade = string.to_i
   if @roster.keys.include?(grade) == false
      @roster[grade] << student 
     else 
