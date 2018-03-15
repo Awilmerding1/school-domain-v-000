b9c045c38fb21cc4bdbe31fb9a5b2d4892249780
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-15 18:56:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,7 @@ class School
   if @roster.keys.include?(grade) == false
      @roster[grade] << student 
     elsif @roster.keys.include?(grade) == true 
-    existing_grade = grade
+    @roster.find
     @roster[existing_grade] << student
  end
 end
